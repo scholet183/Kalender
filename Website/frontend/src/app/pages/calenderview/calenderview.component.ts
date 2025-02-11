89bc3474c0ada981733c401123488796c3ad6388
Patrice Teuber <patrice.teuber@gmail.com>
2025-02-11 14:26:20 +0100

    added first login implementation

---
+++ Website/frontend/src/app/pages/calenderview/calenderview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calenderview',
+  imports: [],
+  templateUrl: './calenderview.component.html',
+  styleUrl: './calenderview.component.css',
+  standalone: true
+})
+export class CalenderviewComponent {
+
+}
