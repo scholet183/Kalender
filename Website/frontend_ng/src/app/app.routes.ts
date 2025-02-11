6016d7dbf59e95bd2f9f9c01547d4121eaaa8f75
Tim Schöler <tim_schoeler@baxter.com>
2025-02-11 18:03:02 +0100

    changed build parameters

--- Website/frontend_ng/src/app/app.routes.ts
+++ Website/frontend_ng/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
-export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  // Weitere Routen hier
-];
+export const routes: Routes = [];
