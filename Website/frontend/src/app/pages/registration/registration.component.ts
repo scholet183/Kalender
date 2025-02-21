2491c34b2a310f9a55b5e3c8d6013498ed1ec2f7
Patrice Teuber <patrice.teuber@gmail.com>
2025-02-21 21:45:51 +0100

    refactored code analysis warnings

--- Website/frontend/src/app/pages/registration/registration.component.ts
+++ Website/frontend/src/app/pages/registration/registration.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
-import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatButton} from "@angular/material/button";
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
-import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {Router} from "@angular/router";
 import {UserService} from "../../services/user.service";
-import {UserDTO} from "../../models/userDTO.model";
 
 @Component({
   selector: 'app-registration',
