881c544b7b8613aaf86c4d006e6602c4017e2835
Patrice Teuber <patrice.teuber@gmail.com>
2025-02-21 23:25:18 +0100

    added calendar service

---
+++ Website/frontend/src/app/services/calendarService/calendar.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Appointment} from "../../models/apointment.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalendarService {
+  private baseUrl = 'http://localhost:8081/api/calendar';
+
+  constructor(private http: HttpClient) {}
+
+  getAppointments(userId: number): Observable<Appointment[]> {
+    const url = `${this.baseUrl}/getAppointments`;
+    return this.http.get<Appointment[]>(url, { params: { userId: userId.toString() } });
+  }
+
+  addAppointment(appointment: Appointment): Observable<Appointment> {
+    const url = `${this.baseUrl}/addAppointment`;
+    return this.http.post<Appointment>(url, appointment);
+  }
+}
