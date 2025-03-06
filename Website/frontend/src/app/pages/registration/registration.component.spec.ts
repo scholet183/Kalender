b46d66f8717f61c9002c6e16d85e466da98fd432
Patrice Teuber <patrice.teuber@gmail.com>
2025-03-06 08:06:40 +0100

    removed login and registermethods that throws fake errors

--- Website/frontend/src/app/pages/registration/registration.component.spec.ts
+++ Website/frontend/src/app/pages/registration/registration.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
-import { of, throwError } from 'rxjs';
+import { of } from 'rxjs';
 import { RegistrationComponent } from './registration.component';
 import { UserService } from '../../services/userService/user.service';
 
@@ -59,21 +59,6 @@ describe('RegistrationComponent', () => {
     // Assert
     expect(userServiceSpy.addUser).toHaveBeenCalledWith('Test User', 'test@example.com', 'password123');
     expect(routerSpy.navigate).toHaveBeenCalledWith(['/login']);
-  });
-
-  it('should set errorMessage if registration fails', () => {
-    // Arrange: Set valid input values but simulate an error from the addUser call
-    component.name = 'Test User';
-    component.email = 'test@example.com';
-    component.password = 'password123';
-    userServiceSpy.addUser.and.returnValue(throwError(() => new Error('Registration error')));
-
-    // Act
-    component.onSubmit();
-
-    // Assert
-    expect(userServiceSpy.addUser).toHaveBeenCalledWith('Test User', 'test@example.com', 'password123');
-    expect(component.errorMessage).toBe('Benutzererstellung fehlgeschlagen.');
   });
 
   it('should navigate to login when routeToLogin is called', () => {
