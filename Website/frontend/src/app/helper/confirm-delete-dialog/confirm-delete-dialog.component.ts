8ff23a1e9591a88918750454271ea23766e25c88
Patrice Teuber <patrice.teuber@gmail.com>
2025-04-10 18:31:14 +0200

    added confirmation dialog

---
+++ Website/frontend/src/app/helper/confirm-delete-dialog/confirm-delete-dialog.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-confirm-delete-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+  templateUrl: './confirm-delete-dialog.component.html'
+})
+export class ConfirmDeleteDialogComponent {
+  constructor(private dialogRef: MatDialogRef<ConfirmDeleteDialogComponent>) {}
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
+}
